44a8b6ac9c1a6cac995f3e8e015216545b65b18a
Ritika <shiritika15031999@gmail.com>
2022-10-17 11:00:04 +0530

    soem chenges made

--- script.js
+++ script.js
@@ -4,12 +4,11 @@
 let input = document.querySelectorAll(".input"); //selecting all input fields
 let button = document.querySelectorAll(".button"); //selecting all buttons
 
-for(var i = 0 ; i < button.length ; i++){ //for each button it must be disabled
+for (var i = 0; i < button.length; i++) { //for each button it must be disabled
   button[i].disabled = true; //setting button state to disabled
 }
 
-
-input[0].addEventListener("change", swapHandle);//for input field value
+input[0].addEventListener("change", swapHandle); //for input field value
 input[1].addEventListener("change", swapHandle);
 input[2].addEventListener("change", frequencyHandle);
 input[3].addEventListener("change", anagramHandle);
@@ -20,61 +19,60 @@ input[7].addEventListener("change", searchingHandle);
 input[8].addEventListener("change", searchHandle);
 
 function swapHandle() {
-    if (document.getElementById("fval").value === "") {
-        button[0].disabled = true; //button remains disabled
-    } else {
-        button[0].disabled = false; //button is enabled
-    }
+  if (document.getElementById("fval").value === "") {
+    button[0].disabled = true; //button remains disabled
+  } else {
+    button[0].disabled = false; //button is enabled
+  }
 }
 
 function frequencyHandle() {
   if (document.getElementById("input_frequency").value === "") {
-      button[1].disabled = true; //button remains disabled
+    button[1].disabled = true; //button remains disabled
   } else {
-      button[1].disabled = false; //button is enabled
+    button[1].disabled = false; //button is enabled
   }
 }
 
 function anagramHandle() {
   if (document.getElementById("firstInput").value === "") {
-      button[2].disabled = true; //button remains disabled
+    button[2].disabled = true; //button remains disabled
   } else {
-      button[2].disabled = false; //button is enabled
+    button[2].disabled = false; //button is enabled
   }
 }
 
 function eopHandle() {
   if (document.getElementById("inputNumber").value === "") {
-      button[3].disabled = true; //button remains disabled
+    button[3].disabled = true; //button remains disabled
   } else {
-      button[3].disabled = false; //button is enabled
+    button[3].disabled = false; //button is enabled
   }
 }
 
 function sortingHandle() {
   if (document.getElementById("sort_input").value === "") {
-      button[4].disabled = true; //button remains disabled
+    button[4].disabled = true; //button remains disabled
   } else {
-      button[4].disabled = false; //button is enabled
+    button[4].disabled = false; //button is enabled
   }
 }
 
 function searchingHandle() {
   if (document.getElementById("search_input").value === "") {
-      button[5].disabled = true; //button remains disabled
+    button[5].disabled = true; //button remains disabled
   } else {
-      button[5].disabled = false; //button is enabled
+    button[5].disabled = false; //button is enabled
   }
 }
 
 function searchHandle() {
   if (document.getElementById("value_input").value === "") {
-      button[6].disabled = true; //button remains disabled
+    button[6].disabled = true; //button remains disabled
   } else {
-      button[6].disabled = false; //button is enabled
+    button[6].disabled = false; //button is enabled
   }
 }
-
 
 //-----------------------code for swapping-----------------------------------
 const btnSwap = document.getElementById("btn1"); //get button swap
@@ -89,9 +87,6 @@ function swapText(event) {
   temp = first_value;
   first_value = second_value;
   second_value = temp;
-
-  console.log(first_value);
-  console.log(second_value);
 
   //saving swap value again
   document.getElementById("fval").value = first_value;
@@ -108,19 +103,17 @@ function freqencyCount(event) {
   var string_value = document.getElementById("input_frequency").value;
 
   const string_split = string_value.split(""); //converting string to array using split method
-  console.log(string_split);
 
   //we use forEach method to access each element in array and count it
   let count = {}; //here we init count by setting it as empty obj it will tell count of each element
   string_split.forEach((val, index) => {
-    if (!count[val]) {
-      //if element comes only first time
+    if (!count[val]) { //if element comes only first time
       count[val] = 1;
     } else {
       count[val] = count[val] + 1; //if element come more that one time count get increased by 1
     }
   });
-  console.log(count);
+
   let str_count = JSON.stringify(count); //converting obj to string to get display
   console.log(str_count);
 
@@ -134,25 +127,19 @@ function checkAnagram(event) {
   event.preventDefault();
 
   var string1 = document.getElementById("firstInput").value; //get value from input box
-  // console.log(string1);
-
   var string2 = document.getElementById("secondInput").value; //get another string(sorted of first string)
-  // console.log(string2);
   const check_Anagram = isAnagram(string1, string2);
 
   function isAnagram(string1, string2) {
-    if (string1.length !== string2.length) {
-      //check the lengtrh of both strings equal or not
+    if (string1.length !== string2.length) {  //check the lengtrh of both strings equal or not
       return false; //return false if length is not equal
     }
     var counter = {}; //empty obj for frequency count of string1
     for (let letter of string1) {
       counter[letter] = (counter[letter] || 0) + 1; //if not set 0 it will give undefine
     }
-    for (let items of string2) {
-      //for each letter in string2
-      if (!counter[items]) {
-        //if any letter is not in privious counter obj return false
+    for (let items of string2) { //for each letter in string2
+      if (!counter[items]) { //if any letter is not in privious counter obj return false
         return false;
       }
       counter[items] = counter[items] - 1; //otherwise conter item get minus to become undefined
@@ -161,11 +148,9 @@ function checkAnagram(event) {
   }
   if (check_Anagram === false) {
     document.getElementById("output").innerHTML =
-      "NO, it is not an Anagram! :("; //display obj on window
-    // console.log("NO, it is not an Anagram");
+      "NO, it is not an Anagram! :("; //display obj on window // console.log("NO, it is not an Anagram");
   } else {
     document.getElementById("output").innerHTML = "YES, it is an Anagram! :)";
-    // console.log("YES, it is Anagram");
   }
 }
 
@@ -176,49 +161,41 @@ function checkEOP(event) {
   event.preventDefault();
 
   let stringInput = document.getElementById("inputNumber").value; //get value from input box in form if string
-  console.log(stringInput);
   //validation to check input value should be a number
-  if(isNaN(stringInput)){
-    document.getElementById("even").innerHTML ="Please inter only numbers!**"
-  } else{
-    
+  if (isNaN(stringInput)) {
+    document.getElementById("even").innerHTML = "Please inter only numbers!**";
+  } else {
     let array = stringInput.split(""); //convert string input in array
-  console.log(array);
 
-  var arr = ["Even:"]; 
-  var arr2 = ["Odd:"];
-  var arr3 = ["Prime:"];
+    var arr = ["Even:"];
+    var arr2 = ["Odd:"];
+    var arr3 = ["Prime:"];
 
-  for (let i = 0; i < array.length; i++) { // code for checking even odd
-    if (array[i] % 2 == 0) {
-      arr.push(array[i]);
-    } else if (array[i] % 2 != 0) {
-      arr2.push(array[i]);
-    } 
-  }
+    for (let i = 0; i < array.length; i++) { // code for checking even odd
+      if (array[i] % 2 == 0) {
+        arr.push(array[i]);
+      } else if (array[i] % 2 != 0) {
+        arr2.push(array[i]);
+      }
+    }
 
-for (let i = 0; i < array.length ; i++) { // code for checking prime number
-    let flag = 0;
-    for (let j = 2; j < array[i]; j++) {
+    for (let i = 0; i < array.length; i++) { // code for checking prime number
+      let flag = 0;
+      for (let j = 2; j < array[i]; j++) {
         if (array[i] % j == 0) {
-            flag = 1;
-            break;
+          flag = 1;
+          break;
         }
-    }
-     // if number greater than 1 and not divisible by other numbers
-    if (array[i] > 1 && flag == 0) {
+      }
+      // if number greater than 1 and not divisible by other numbers
+      if (array[i] > 1 && flag == 0) {
         arr3.push(array[i]);
+      }
     }
+    document.getElementById("even").innerHTML = arr;
+    document.getElementById("odd").innerHTML = arr2;
+    document.getElementById("prime").innerHTML = arr3;
   }
-
-  console.log(arr);
-  console.log(arr2);
-  console.log(arr3);
-
-  document.getElementById('even').innerHTML = arr;
-  document.getElementById('odd').innerHTML = arr2;
-  document.getElementById('prime').innerHTML = arr3;
-}
 }
 
 //-----------------------code for Sorting starts-------------------------------------
@@ -228,32 +205,38 @@ function checkSorting(event) {
   event.preventDefault();
 
   let sort_Input = document.getElementById("sort_input").value; //get value from input box in form if string
-  console.log(sort_Input);
+  var array = sort_Input.split(""); //convert string input in array
 
-  //validation to check input value should be a number
-  if(isNaN(sort_Input)){
-    document.getElementById("invalid7").innerHTML ="Please inter only numbers!**"
-    var result = sort_Input.split(/[, ]+/);
-    console.log(result);
-    var sorted_array = result.sort();
+  var sortedArray = sortArray(array);
 
-  } else{
-    var array = sort_Input.split(""); //convert string input in array
-    // var sorted_array = array.sort(function(a, b){return a - b});//sort() function sorts values as strings
+  // validation to check input value should be a number
+  // var sorted_array;
+  // if(isNaN(sort_Input)) {
+  //   document.getElementById("invalid7").innerHTML ="Please inter only numbers!**"
+  //   var result = sort_Input.split(/[, ]+/);
+  //   console.log(result);
+  //   sorted_array = result.sort();
+  // } else {
+  //   var array = sort_Input.split(""); //convert string input in array
+  //   //sorted_array = array.sort(function(a, b){return a - b});//sort() function sorts values as strings
+  //   sortArray(array);
+  // }
 
-    //sort array with for loop
-    for (let i = 1 ; i < array.length; i++) // for next value
-    for (let j = 0; j < i; j++)             //for prev value
-        if (array[i] < array[j]) {          //comparison between next & prev value   
-          let x = array[i]; //concept to swap values
-          array[i] = array[j];
-          array[j] = x;
-        }
-        sorted_array = array;
-      }
-      document.getElementById("invalid7").innerHTML = `[ Sorted Array: ${sorted_array}]`;
+  document.getElementById("invalid7").innerHTML = `[ Sorted Array: ${sortedArray}]`;
 }
-
+//sort array with for loop
+function sortArray(array) {
+  for (let i = 1; i < array.length; i++) { // for next value
+    for (let j = 0; j < i; j++) { //for prev value
+      if (array[i] < array[j]) { //comparison between next & prev value
+        let x = array[i]; //concept to swap values
+        array[i] = array[j];
+        array[j] = x;
+      }
+    }
+  }
+  return array;
+}
 //-----------------------code for sorting end here-------------------------------
 
 //-----------------------code for searching start-----------------------------
@@ -266,14 +249,14 @@ function getValue(event) {
   console.log(search_Input);
 
   //validation to check input value should be a number
-  breakme :if(isNaN(search_Input)){
-    document.getElementById("eror").innerHTML ="Please enter only numbers!**"
+  breakme: if (isNaN(search_Input)) {
+    document.getElementById("eror").innerHTML = "Please enter only numbers!**";
     break breakme;
   } else {
     var array = search_Input.split(""); //convert string input in array
     iteration(array);
   }
-  
+
   // function iteration(array){
   //   let search_Value = document.getElementById("value_input").value;
   //   console.log(search_Value);
@@ -284,25 +267,28 @@ function getValue(event) {
   //     document.getElementById("output5").innerHTML = "NOT Present";
   //   }
   // }
-function iteration(array){
-  let search_Value = document.getElementById("value_input").value;
-  console.log(search_Value);
+  function iteration(array) {
+    let search_Value = document.getElementById("value_input").value;
+    console.log(search_Value);
+    let flag = 0;
     //iteration method to solve this problem
-    for(let i = 0 ; i < array.length ; i++){
-      if(array[i] === search_Value){
-        document.getElementById("output5").innerHTML = "Yes number is present!";
-        document.getElementById("output1").innerHTML = `Value: ${search_Value}`;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === search_Value) {
+        flag = 1;
         break;
-      } else if(array[i] !== search_Value) {
-        document.getElementById("output5").innerHTML = "NOT Present";
       }
     }
+    if (flag == 1) {
+      document.getElementById("output5").innerHTML = "Yes number is present!";
+      document.getElementById("output1").innerHTML = `Value: ${search_Value}`;
+    } else {
+      document.getElementById("output5").innerHTML = "NOT Present";
+    }
   }
-}
-    
+
   const btn_getposition = document.getElementById("btn7");
-  btn_getposition.addEventListener("click" ,getPosition);
-  function getPosition(event){
+  btn_getposition.addEventListener("click", getPosition);
+  function getPosition(event) {
     let search_Value = document.getElementById("value_input").value;
     event.preventDefault();
     let search_Input = document.getElementById("search_input").value; //get value from input box in form if string
@@ -312,26 +298,27 @@ function iteration(array){
     // // document.getElementById("output2").innerHTML = index;
     // document.getElementById("output2").innerHTML = index + 1;
 
-  //   for(let i = 0 ; i < obj.length ; i++){
-  //     if(obj[i] === search_Value){
-  //       let index = new_Array.indexOf(search_Value);
-  //    document.getElementById("output2").innerHTML = index + 1;
-  //   }
-  // }
-  
-  //accessing object approach
+    //   for(let i = 0 ; i < obj.length ; i++){
+    //     if(obj[i] === search_Value){
+    //       let index = new_Array.indexOf(search_Value);
+    //    document.getElementById("output2").innerHTML = index + 1;
+    //   }
+    // }
+
+    //accessing object approach
     var obj = {};
-    for (let i = 0; i < new_Array.length; ++i){ //for inserting array into object
-       obj[i] = new_Array[i];
-      }
-      let new_obj = obj;
-      
-      //loop for iterating object
-      for(const item in new_obj){ 
-        var key = Object.keys(new_obj)[item];
-        if(new_obj[key] === search_Value){
-          document.getElementById("output2").innerHTML = key;
-        }
-      }  
+    for (let i = 0; i < new_Array.length; ++i) { //for inserting array into object
+      obj[i] = new_Array[i];
     }
-  
+    let new_obj = obj;
+
+    //loop for iterating object
+    for (const item in new_obj) {
+      var key = Object.keys(new_obj)[item];
+      if (new_obj[key] === search_Value) {
+        let keyNew = parseInt(key);
+        document.getElementById("output2").innerHTML = keyNew + 1;
+      }
+    }
+  }
+}
